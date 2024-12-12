ab44cca6e4af9d47c482737839f6cf1622c751e5
Anwar S <anwarsaifullah+code@yandex.com>
2024-12-12 10:59:03 +0700

    trying out things

--- app/src/main/java/com/audz/MainActivity.java
+++ app/src/main/java/com/audz/MainActivity.java
@@ -2,12 +2,15 @@ package com.audz;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    final TextView helloTextView = (TextView) findViewById(R.id.text_view_id);
+    helloTextView.setText(R.string.user_greeting);
   }
 }
 
