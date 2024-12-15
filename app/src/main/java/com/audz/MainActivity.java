f399cdcaa16934e2ba62b4c3a30e6b8098b6e25b
Anwar S <anwarsaifullah+code@yandex.com>
2024-12-15 09:30:52 +0700

    add styled switch buttons

--- app/src/main/java/com/audz/MainActivity.java
+++ app/src/main/java/com/audz/MainActivity.java
@@ -3,19 +3,38 @@ package com.audz;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.content.Intent;
 
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.content.Context;
 import android.view.accessibility.AccessibilityManager;
 
+import android.widget.CompoundButton;
+import com.google.android.material.materialswitch.MaterialSwitch;
+
 public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    setTheme(R.style.AppTheme);
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    final TextView helloTextView = (TextView) findViewById(R.id.text_view_id);
-    helloTextView.setText(R.string.user_greeting);
+    // final TextView helloTextView = (TextView) findViewById(R.id.text_view_id);
+    // helloTextView.setText(R.string.user_greeting);
+
+    MaterialSwitch materialSwitch = findViewById(R.id.materialSwitch);
+
+        // Set listener for switch toggle
+    materialSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (isChecked) {
+                Toast.makeText(MainActivity.this, "Switch ON", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Switch OFF", Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
 
     // Intent intent = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
     // startActivity(intent);
